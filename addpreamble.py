fefa03e43c5676250d2f1b98f253a689ecdf7ce4
Bethany Macri <bethany.macri@gmail.com>
2012-07-21 15:15:45 -0400

    Script prepends 'preamble' to all .py files.  If first line of file is #!/usr/bin/env python, the script inserts 'preamble' after the first line but before the rest of the lines in the file.

---
+++ addpreamble.py
@@ -0,0 +1,30 @@
+import os
+
+def add_preamble(current_directory):
+	for path, names, files in os.walk(current_directory):
+		for afile in files:
+			afile_path = os.path.join(path, afile)
+			if afile_path.endswith('.py'):
+				f = open(afile_path, 'r')
+				all_lines = f.readlines()		
+				first_line = all_lines[0]
+				if first_line == '#!/usr/bin/env python\n':
+					all_lines = f.readlines()
+					f.close()
+					f = open(afile_path, 'w')
+					f.write(first_line)
+					f.write(all_lines[1:])
+					f.write("preamble\n\n")
+					f.close()				
+				elif first_line != '#!/usr/bin/env python':
+					old_contents = f.read()
+					f = open(afile_path, 'w')
+					f.write("preamble\n\n")
+					f.write(old_contents)
+					f.close()
+
+
+
+
+#--------------------------------------------------------------------------------
+add_preamble('.') 
