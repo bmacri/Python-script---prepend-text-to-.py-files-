1dcbfd529f1504d508c7453b588d8be2883015ac
Bethany Macri <bethany.macri@gmail.com>
2012-08-21 13:54:58 -0400

    Commit to update

--- addpreamble.py
+++ addpreamble.py
@@ -7,22 +7,23 @@ def add_preamble(current_directory):
                 afile_path = os.path.join(path, afile)
                 if afile_path.endswith('.py') and afile != 'addpreamble.py':
                     f = open(afile_path, 'r')
-                    all_lines = f.readlines()		
-				    first_line = all_lines[0]
-				    if first_line == '#!/usr/bin/env python\n':
-					    all_lines = f.readlines()
-					    f.close()
-					    f = open(afile_path, 'w')
-					    f.write(first_line)
-					    f.write(all_lines[1:])
-					    f.write("preamble\n\n")
-					    f.close()				
-				    elif first_line != '#!/usr/bin/env python':
-					    old_contents = f.read()
-					    f = open(afile_path, 'w')
-					    f.write("preamble\n\n")
-					    f.write(old_contents)
-					    f.close()
+                    all_lines = f.readlines()
+                    if all_lines != []:		
+                        first_line = all_lines[0]
+                        if first_line == '#!/usr/bin/env python\n' or first_line == 'coding: utf-8':
+                            all_lines = f.readlines()
+                            f.close()
+                            f = open(afile_path, 'w')
+                            f.write(first_line)
+                            f.write(str(all_lines[1:]))
+                            f.write("preamble\n\n")
+                            f.close()				
+                        elif first_line != '#!/usr/bin/env python':
+                            old_contents = f.read()
+                            f = open(afile_path, 'w')
+                            f.write("preamble\n\n")
+                            f.write(old_contents)
+                            f.close()
 
 
 
